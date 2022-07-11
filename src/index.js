c0c295f89316b1d71905faa85a105528a3363161
cayenh <cayenh@concredito.com.mx>
2022-07-11 18:55:18 -0500

    se corrigio

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const getGeoData = (ip, ub) => {
     let geoInfo = {};
     axios.get(`http://ip-api.com/json/${ip}`)
         .then(response => {
-            console.log('ubicacion en funcion', ub);
+            console.log('ubicacion en funcion', ub.coords);
 
 
             //response.data.latitudReal = ub.coords.latitude;
